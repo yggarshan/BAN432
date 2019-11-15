71c9bccac2234414a0041a81bffb2f01ef66df01
Yngve <yngve.sture@gmail.com>
2019-11-15 19:44:02 +0100

    Annotation

--- kwic.R
+++ kwic.R
@@ -106,75 +106,12 @@ sur.words.new <- paste(sur.words, collapse="")
 sur.words.new.split <- strsplit(sur.words.new, " ")
 
 
-###### Turn list of old words into vector
-#### Old IPOs (2009) ####
-ind_old <- grep("-2009-", filenames_full)
-filenames_old <- filenames_full[ind_old]
-
-txt_old <- list()
-
-for (i in 1:length(filenames_old)){
-  txt_old[[i]] <- readLines(paste0(getwd(),"/ipos_2nd_qtr_2008_2019_nouns_adj/", filenames_old[i]))
-}
-
-txt_old <- remove_duplicates(txt_old) #deleting duplicate reports
-
-# turn into one large string of words
-all_old <- paste(txt_old, collapse=" ")     # Combine all texts to one large string
-all_old <- tolower(all_old)                 # Make all words lower case
-all_old <- removePunctuation(all_old)       # Remove symbols
-
-#Tokanize the list 
-old_tok <- sapply(txt_old, function(i) scan(text = i,
-                                            what = "character",
-                                            quote = ""))
-
-#Remove unnecessary punctuation and apply tolower
-old_tok <- sapply(old_tok, function(i) removePunctuation(i))
-old_tok <- sapply(old_tok, function(i) gsub(x = i, 
-                                            pattern = "nn|nns|jj|ii|nnp", 
-                                            replacement = " "))
-
-#Create a list of terms for the relevant technology terms
-dic.emerg <- "emerging|technology|patent|disruptive technology"
-
-#Locate the terms from dic.emerg in old_tok
-index.emerg <- unique(sapply(old_tok, function(i) unique(grep(dic.emerg, x = i ))))
-
-#Run KWIC analysis and save results to a list 
-result <- list()
-sur.words <- c()
-
-for(i in 1:length(index.emerg)){
-  result[[i]] <- make.KWIC(index.emerg[[i]],
-                           old_tok[[i]],
-                           n = 10,
-                           doc.nr = i)
-  
-  sur.words[i] <- paste((result[[i]]$surrounding), collapse=" ") # surrounding words
-}
-
-sur.words.old <- unlist(paste(sur.words, collapse=" "))
-sur.words.old.split <- strsplit(sur.words.old, " ")
 
 # create a df where all words are counted in each of the data sets
 df.new <- as.data.frame(table(sur.words.new.split))
-df.old <- as.data.frame(table(sur.words.old.split))
-
 colnames(df.new) <- c('token', 'count')
-colnames(df.old) <- c('token', 'count')
-
 df.new$age <- rep("new", nrow(df.new))
-df.old$age <- rep("old", nrow(df.old))
-
-# Turning the two df's into one combined df 
-df.full <- rbind(df.new, df.old)
-df.full <- spread(df.full, age, count)
 df.full$new <- ifelse(is.na(df.full$new), 0, df.full$new)
-df.full$old <- ifelse(is.na(df.full$old), 0, df.full$old)
-
-
-
 
 # df.full$token <- unlist(lapply(df.full$token, function(x) gsub(x=x,"([^a-zA-Z0-9])", ""))) # removing noise
 
