6bf31bf4c6a3da351abb493a95dd844c068b4139
Yngve <yngve.sture@gmail.com>
2019-11-15 12:22:47 +0100

    Changed make.kwic code to surrounding instead of left and right

--- kwic.R
+++ kwic.R
@@ -44,128 +44,60 @@ all_new <- paste(txt_new, collapse=" ")     # Combine all texts to one large str
 all_new <- tolower(all_new)                 # Make all words lower case
 all_new <- removePunctuation(all_new)       # Remove symbols
 
-############################# Assignment 2 ###########################
-
-
-# split relevant part of the vector at "|"
-edgar.index <- strsplit(edgar.index.raw[12:length(edgar.index.raw)], 
-                        split = "|", 
-                        fixed = T)
-# make a matrix
-edgar.index <- matrix( unlist(edgar.index), ncol = 5, byrow = T)
-
-
-# make a data.frame
-edgar.index <- data.frame(edgar.index, stringsAsFactors = F)
-
-# column names
-colnames(edgar.index) <- c("cik", "company.name", "form.type", "date.filed", "url")
-
-# Limit to 10K
-edgar.index <- edgar.index[edgar.index$form.type == "10-K", ]
-
-# Extract sustainable companies from 'comp'
-comp.sus <- comp[comp[,3]==T,]
-
-# Limit to sustainable companies
-edgar.sus <- edgar.index[edgar.index$cik %in% comp.sus$cik,]
-
-# Define file path
-edgar.sus$file.path <- paste0("tenK", 1:69, ".txt")
-
-# Step 2: loop and download/save 
-
-for(i in 1:nrow(edgar.sus)){
-  # download.file
-  download.file(url = paste0("https://www.sec.gov/Archives/", 
-                             edgar.sus$url[i]), 
-                destfile = edgar.sus$file.path[i])
-}
-
-##################################
-####TASK 2########################
-##################################
-
-# We created a loop that sequentially,
-#Reads in a text as tenK. 
-#We assume that the actual 10K is located between the first time <TEXT> and </TEXT> are mentioned. Hence, we extract that part
-#Then we remove all html-tags in the form of <html-tag>
-#We remove all punctuation and all numbers
-#We proceed to removing excessive whitespace: before, after and within a string.
-#We remove empty strings
-#Finally, we save the next clean text file 
-
-for (i in 1:69) {
-  tenK <- readLines(paste0("tenK",i,".txt"))
-  
-  First <- grep("<TEXT>",tenK)
-  Last <- grep("</TEXT>", tenK)
-  extract <- tenK[First[1]:Last[1]]
-  
-  no_html <- gsub("<.*?>","",extract)
-  no_entities <- gsub("&.+?;","",no_html)
-  no_punc  <- gsub("[^[:alnum:]]", " ", no_entities)
-  no_numbers <- removeNumbers(no_punc)
-  extract_final<- gsub("^\\s+|\\s+$","", no_numbers)
-  extract_final <- gsub("\\s+"," ",extract_final)
-  extract_final <- extract_final[extract_final != ""] 
-  
-  write.csv(extract_final, file = paste0("clean_10K_",i,".txt"), row.names=FALSE)
-  print(i)
-}
-
 
 ##################################
 ####TASK 3########################
 ##################################
 
-# load in cleaned 10k text files into a list 
-
-ten_K_text <- list()
-for (i in 1:69){
-  ten_K_text[[i]] <- readLines(paste0("clean_10K_",i,".txt"))
-}
-
 #Tokanize the list 
-
-business.des <- sapply(ten_K_text, function(i) scan(text = i,
+new_tok <- sapply(txt_new, function(i) scan(text = i,
                                                     what = "character",
                                                     quote = ""))
-#Remove unnecessary punctuation and apply tolower
 
-business.des <- sapply(business.des, function(i) removePunctuation(i))
-business.des <- sapply(business.des, function(i) tolower(i))
+#Remove unnecessary punctuation and apply tolower
+new_tok <- sapply(new_tok, function(i) removePunctuation(i))
+new_tok <- sapply(new_tok, function(i) tolower(i))
 
 #Create a list of terms for the sustainability score
 
-dic.env <- "sustainab|carbon|environment|eco system|pollut|emission|climate change|stakeholder|ecologic|recycle|warming|planet|community|waste|renewable|communit"
+dic.emerg <- "emerging technology|technology|emerging|patent|disruptive technology"
 
-#Locate the terms from dic.env in business.des
-index.env <- unique(sapply(business.des, function(i) unique(grep(dic.env, x = i ))))
+#Locate the terms from dic.emerg in business.des
+index.emerg <- unique(sapply(new_tok, function(i) unique(grep(dic.emerg, x = i ))))
 
 #Create KWIC function
 require(dplyr)
 make.KWIC <- function(index.env, business.des, n, doc.nr){
-  KWIC <- tibble(left = sapply(index.env,
-                               function(i) {paste(business.des[(i-n):(i-1)], collapse = " ")}),
-                 keyword = business.des[index.env], 
-                 right = sapply(index.env,
-                                function(i) {paste(business.des[(i+1):(i+n)], collapse = " ")}),
+  KWIC <- tibble(keyword = business.des[index.env], 
+                 surrounding = sapply(index.env,
+                                      function(i) {paste(business.des[c(((i-n):(i-1)), ((i+1):(i+n)))],collapse = " ")}),
                  doc.nr = doc.nr,
                  position.in.text = index.env/(length(business.des)*0.01))
   return(KWIC)
 }
 
 #Run KWIC analysis and save results to a list 
-
 result <- list()
-for(i in 1:length(business.des)){
-  result[[i]] <- make.KWIC(index.env[[i]],
-                           business.des[[i]],
+for(i in 1:108){
+  result[[i]] <- make.KWIC(index.emerg[[i]],
+                           new_tok[[i]],
                            n = 5,
                            doc.nr = i)
 }
 
 #Look at the result
-result[[1]]
+result[[1]]$left[1]
+result[[1]]$right[1]
+result[[1]]$surrounding[1]
 result[[5]]
+
+unite(result[[5]]$left,)
+
+test3 <- paste(paste((result[[5]]$left), collapse=""),
+              paste((result[[5]]$right), collapse=""))
+              
+              
+test1 <- paste(c(result[[5]]$right), collapse="")
+test2 <- paste(test,test1)
+
+head(test2)
