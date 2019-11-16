a771aa134d99f63cbeda04459f2fdd8386ea91e4
Yngve <yngve.sture@gmail.com>
2019-11-16 18:53:32 +0100

    added cosine file

---
+++ cosine.R
@@ -0,0 +1,6 @@
+# Set current working directory
+setwd('C:/Users/Yngve/Google Drive/Skolerelatert/NHH/Master/BAN432/Final exam')
+
+#### Extract file names ####
+filenames_full <- list.files(path= paste0(getwd(),"/ipos_2nd_qtr_2008_2019_nouns_adj"))
+
