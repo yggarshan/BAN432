8e29a221f4445102b05b08007dffd6588c0aae7a
Yngve <yngve.sture@gmail.com>
2019-11-16 11:28:32 +0100

    Merged part of Vegards's code with my kwic code

--- kwic.R
+++ kwic.R
@@ -21,6 +21,7 @@ make.KWIC <- function(index.env, business.des, n, doc.nr){
   return(KWIC)
 }
 
+
 # Function to remove duplicates text files
 remove_duplicates <- function(text_list){
   rows_delete = c()
@@ -42,33 +43,38 @@ remove_duplicates <- function(text_list){
   final_text <- text_list[-c(final_delete)]
 }
 
+
 #### Extract file names ####
 filenames_full <- list.files(path= paste0(getwd(),"/ipos_2nd_qtr_2008_2019_nouns_adj"))
 
-#### New IPOs (2019) ####
-ind_new <- grep("-2019-|-2018-|-2017-|-2016-", filenames_full)
-filenames_new <- filenames_full[ind_new]
 
+#### New IPOs (2019) ####
+ind_new <- grep("-2019-", filenames_full)
+filenames_new <- filenames_full[ind_new]
 txt_new <- list() # create list where IPO reports will be stored
+
 
 # Extract all files in working directory that matches specified year(s)
 for (i in 1:length(filenames_new)){
   txt_new[[i]] <- readLines(paste0(getwd(),"/ipos_2nd_qtr_2008_2019_nouns_adj/", filenames_new[i]))
 }
 
+
 # Remove duplicate files with own function
-txt_new <- remove_duplicates(txt_new) # remove duplicate files with 
+txt_new <- remove_duplicates(txt_new) # remove duplicate files with own created function
 
 
-###### Turn list of new words into vector #####
+###### Turn list of new words into vector
 # turn into one large string of words
 all_new <- paste(txt_new, collapse=" ")     # Combine all texts to one large string
 all_new <- tolower(all_new)                 # Make all words lower case
+
 
 #Tokanize the list of new IPO reports 
 new_tok <- sapply(txt_new, function(i) scan(text = i,
                                                     what = "character",
                                                     quote = ""))
+
 
 # Data cleaning of vector of new words
 new_tok <- sapply(new_tok, function(i) removePunctuation(i))              # Remove punctuation
@@ -77,8 +83,10 @@ new_tok <- sapply(new_tok, function(i) gsub(x = i,                        # Remo
                                             pattern = "nn|nns|jj|ii|nnp", 
                                             replacement = " "))
 
+
 #Create a list of terms of relevant technology terms
 dic.emerg <- "emerg|technolog|leading"
+
 
 #Locate the terms from dic.emerg in business.des
 index.emerg <- unique(sapply(new_tok, function(i) unique(grep(dic.emerg, x = i ))))
@@ -87,6 +95,7 @@ index.emerg <- unique(sapply(new_tok, function(i) unique(grep(dic.emerg, x = i )
 #Run KWIC analysis and save results to a list 
 result <- list() # create new list to store KWIC results 
 sur.words <- c() # create vector to store surrounding words
+
 
 for(i in 1:length(index.emerg)){ # run KWIC analysis
   result[[i]] <- make.KWIC(index.emerg[[i]], # report index
@@ -98,6 +107,7 @@ for(i in 1:length(index.emerg)){ # run KWIC analysis
                                                                  # list of all words found
 }
 
+# Clean the list of surrounding words
 sur.words.new <- paste(sur.words, collapse="") # combine all surrounding words into one string
 sur.words.new.split <- unlist(strsplit(sur.words.new, " ")) # tokenize (split) all words from string into vector
 sur.words.new.split <- tolower(sur.words.new.split)
@@ -118,10 +128,76 @@ df.new <- df.new %>% filter(token != "", nchar(token) > 2, count>1, count<150)
 df.new <- as.data.frame(table(sur.words.new.split))
 colnames(df.new) <- c('token', 'count')
 df.new$age <- rep("new", nrow(df.new))
-df.full$new <- ifelse(is.na(df.full$new), 0, df.full$new)
-
-df.full$token <- sapply(df.full$token, function(x) gsub(x=x,"([^a-zA-Z0-9])", "")) # removing noise
 
 
-rm(list=setdiff(ls(), c("df.new","txt_new", "filenames_n_ad", "new_tok")))
+
+
+#### Old IPOs (2008-2010) ####
+
+
+ind_old <- grep("-2010-|-2009-", filenames_full)
+filenames_old <- filenames_full[ind_old]
+
+txt_old <- list()
+
+for (i in 1:length(filenames_old)){
+  txt_old[[i]] <- readLines(paste0(getwd(),"/ipos_2nd_qtr_2008_2019_nouns_adj/", filenames_old[i]))
+}
+
+txt_new <- remove_duplicates(txt_new) # remove duplicate files with 
+
+# Prepare "old" data set
+txt_old <- unlist(txt_old)
+
+df_old <- data.frame(token=rep(NA,length(txt_old)))
+df_old$token <- txt_old
+
+df_old <- df_old %>% separate(token, c("token", "xpos"), sep="\t")
+df_old <- df_old[-1,]
+
+df_old$token <- tolower(df_old$token)
+df_old$token <- removePunctuation(df_old$token)
+df_old$token <- removeNumbers(df_old$token)
+
+
+df_old <- df_old  %>%
+  group_by(token) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n))
+
+df_old$token <- unlist(lapply(df_old$token, function(x) gsub(x=x,"([^a-zA-Z0-9])", "")))
+
+
+#### Join data frames ####
+df_old$token <- stemDocument(df_old$token)
+df_old <- df_old  %>%
+  filter(token != "" & nchar(token) > 1) %>% 
+  group_by(token) %>% 
+  summarise(count = sum(n)) %>% 
+  arrange(desc(count))
+
+df_new$token <- stemDocument(df_new$token)
+df_new <- df_new  %>%
+  filter(token != "" & nchar(token) > 1) %>%
+  group_by(token) %>% 
+  summarise(count = sum(n)) %>% 
+  arrange(desc(count))
+
+df_new$age <- rep("new", nrow(df_new))
+df_old$age <- rep("old", nrow(df_old))
+
+df_full <- rbind(df_new, df_old)
+df_full <- spread(df_full, age, count)
+df_full$new <- ifelse(is.na(df_full$new), 0, df_full$new)
+df_full$old <- ifelse(is.na(df_full$old), 0, df_full$old)
+df_full <- df_full %>% mutate(diff=new-old)
+df_full$token <- removeWords(df_full$token, stopwords("en"))
+df_full <- df_full %>% filter(token != "", diff > 0)
+
+df_full <- df_full %>% mutate(log_diff = log(abs(diff)))
+
+wordlist <- df_full %>% filter(token %in% df.new$token)
+
+#### Create a weightbin column ####
+
 
